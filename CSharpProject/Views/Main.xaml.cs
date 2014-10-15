512b5f69660a47991465e6f889283fec48e525c4
Tommy <tommyjaks@hotmail.com>
2014-10-15 13:12:42 +0200

    for the win

--- CSharpProject/Views/Main.xaml.cs
+++ CSharpProject/Views/Main.xaml.cs
@@ -1,13 +1,25 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Net;
 using System.Windows;
 
 namespace CSharpProject.Views
 {
     public partial class MainWindow : Window
     {
+        WebClient wc = new WebClient();
         public MainWindow()
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            wc.DownloadFileCompleted += new AsyncCompletedEventArgs(FileWebResponse);
+            var url= new Uri(tbURL.Text);
+            wc.DownloadFileAsync(url, tbFlow.Text);
+        }
+
+        
     }
 }
