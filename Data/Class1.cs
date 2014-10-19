568dad0007935fd400ad01e65611730173527ec9
xtronal <jarvinenemil@hotmail.com>
2014-10-19 22:51:56 +0200

    getRSSFEEDgrunden

--- Data/Class1.cs
+++ Data/Class1.cs
@@ -1,12 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.ServiceModel.Syndication;
 using System.Text;
 using System.Threading.Tasks;
+
 // DET HÄR ÄR EN KOMMENTAR 
+using System.Xml;
+
 namespace Data
 {
-    class Class1
+    internal class Class1
     {
+        private void ParseRssFile()
+        {
+
+            //test url kommer vara (Url i txtboken i xaml.cs samt att den kommer att visas upp i vår lisa
+            string url = "http://tyngreradio.libsyn.com/rss";
+            XmlReader reader = XmlReader.Create(url);
+            SyndicationFeed feed = SyndicationFeed.Load(reader);
+            reader.Close();
+            foreach (SyndicationItem item in feed.Items)
+            {
+                String subject = item.Title.Text;
+                String summary = item.Summary.Text;
+
+            }
+            Console.WriteLine(feed.Title.Text);
+
+        }
     }
 }
