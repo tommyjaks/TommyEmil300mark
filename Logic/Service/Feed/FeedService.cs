a6503861009aefb86c88c35568f647f1a4a631ee
xtronal <jarvinenemil@hotmail.com>
2014-10-24 13:19:21 +0200

    300_KG_Mark_Not_So_Far_Away_1337

--- Logic/Service/Feed/FeedService.cs
+++ Logic/Service/Feed/FeedService.cs
@@ -3,11 +3,25 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Logic.Entities;
+using Logic.Readers;
+using Data;
+using Logic.Repositories;
 
 namespace Logic.Service.Feed
 {
-    class FeedService
+    class FeedService 
     {
+        RssReader feedReader = new RssReader();
 
+
+        private void getRssItems(string url)
+        {
+
+            feedReader.Read(url);
+
+
+
+        }
     }
 }
