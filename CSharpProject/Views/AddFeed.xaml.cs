cefd7c3dce52d64cb7f58dec3fafc6cd5a7cc62d
tommy jaks <tommyjaks@hotmail.com>
2014-11-02 01:57:06 +0100

    saa

---
+++ CSharpProject/Views/AddFeed.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using ComboBox = System.Windows.Controls.ComboBox;
+using MessageBox = System.Windows.MessageBox;
+using TextBox = System.Windows.Controls.TextBox;
+using Logic.Service;
+using Logic.Service.Validation;
+
+namespace CSharpProject.Views
+{
+    /// <summary>
+    /// Interaction logic for AddFeed.xaml
+    /// </summary>
+    public partial class AddFeed : Window
+    {
+        CategoryService fillCategories = new CategoryService();
+        FeedService getFeeds = new FeedService();
+        
+        public AddFeed()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (cbCategory.SelectedItem != null)
+            {
+                var minUrl = tbURL.Text;
+
+                TextBox flowName = tbFlowName;
+                var name = tbFlowName.Text;
+                var category = cbCategory.SelectedItem.ToString();
+                var urlValidate = new UrlValidator();
+
+                if (urlValidate.Validate(minUrl) &&
+                    urlValidate.EmptyTextBox(flowName) == true)
+                {
+                    try
+                    {
+                        getFeeds.getRssItems(minUrl, name, category);
+                        MessageBox.Show("Podcast tillagd!");
+                    }
+                    catch (Exception ez)
+                    {
+                        MessageBox.Show("Din url verkar inte gå att ladda. Felmeddelande:" + ez.Message);
+                    }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Du måste ange en kategori för att spara!");
+            }
+        }
+        private void cbCategory_DropDownOpened_1(object sender, EventArgs e)
+        {
+            cbCategory.Items.Clear();
+            ComboBox cbBoxBox = cbCategory;
+            fillCategories.GetCategory(cbBoxBox);
+        }
+    }
+}
--- CSharpProject/Views/Main.xaml.cs
+++ CSharpProject/Views/Main.xaml.cs
@@ -14,10 +14,7 @@ namespace CSharpProject.Views
 {
     public partial class MainWindow : Window
     {
-       CategoryService fillCategories = new CategoryService();
-        FeedService getFeeds = new FeedService();
-        private ListOfFeeds skapaFeeds = new ListOfFeeds();
-        private Repository<ListOfFeeds> xml2 = new Repository<ListOfFeeds>(); 
+    
 
         
         public MainWindow()
@@ -27,33 +24,34 @@ namespace CSharpProject.Views
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            if (cbCategory.SelectedItem != null)
-            {
-                var minUrl = tbURL.Text;
+        //    if (cbCategory.SelectedItem != null)
+        //    {
+        //        var minUrl = tbURL.Text;
                 
-                TextBox flowName = tbFlowName;
-                var name = tbFlowName.Text;
-                var category = cbCategory.SelectedItem.ToString();
-                var urlValidate = new UrlValidator();
+        //        TextBox flowName = tbFlowName;
+        //        var name = tbFlowName.Text;
+        //        var category = cbCategory.SelectedItem.ToString();
+        //        var urlValidate = new UrlValidator();
 
-                if (urlValidate.Validate(minUrl) &&
-                    urlValidate.EmptyTextBox(flowName) == true)
-                {
-                    try
-                    {
-                        getFeeds.getRssItems(minUrl, name, category);
-                        MessageBox.Show("Podcast tillagd!");
-                    }
-                    catch (Exception ez)
-                    {
-                        MessageBox.Show("Din url verkar inte gå att ladda. Felmeddelande:" + ez.Message);
-                    }
-                }
-            }
-            else
-            {
-                MessageBox.Show("Du måste ange en kategori för att spara!");
-            }
+        //        if (urlValidate.Validate(minUrl) &&
+        //            urlValidate.EmptyTextBox(flowName) == true)
+        //        {
+        //            try
+        //            {
+        //                getFeeds.getRssItems(minUrl, name, category);
+        //                MessageBox.Show("Podcast tillagd!");
+        //            }
+        //            catch (Exception ez)
+        //            {
+        //                MessageBox.Show("Din url verkar inte gå att ladda. Felmeddelande:" + ez.Message);
+        //            }
+        //        }
+        //    }
+        //    else
+        //    {
+        //        MessageBox.Show("Du måste ange en kategori för att spara!");
+        //    }
+            new AddFeed().Show();
         }
 
         private void btnCategory_Click(object sender, RoutedEventArgs e)
@@ -62,12 +60,12 @@ namespace CSharpProject.Views
             
         }
 
-        private void cbCategory_DropDownOpened_1(object sender, EventArgs e)
-        {
-            cbCategory.Items.Clear();
-            ComboBox cbBoxBox = cbCategory;
-            fillCategories.GetCategory(cbBoxBox);
-        }
+        //private void cbCategory_DropDownOpened_1(object sender, EventArgs e)
+        //{
+        //    cbCategory.Items.Clear();
+        //    ComboBox cbBoxBox = cbCategory;
+        //    fillCategories.GetCategory(cbBoxBox);
+        //}
 
         private void btnChangeFeed_Click(object sender, RoutedEventArgs e)
         {
