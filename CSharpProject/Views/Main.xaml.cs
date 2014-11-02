1053ef91322837f1a9f80b990109ddf3937304a7
tommy jaks <tommyjaks@hotmail.com>
2014-11-02 01:58:31 +0100

    rens

--- CSharpProject/Views/Main.xaml.cs
+++ CSharpProject/Views/Main.xaml.cs
@@ -1,13 +1,7 @@
 ﻿
-using System;
+
 using System.Windows;
-using Logic.Entities;
-using Logic.Repositories;
-using Logic.Service.Validation;
-using Logic.Service;
-using ComboBox = System.Windows.Controls.ComboBox;
-using MessageBox = System.Windows.MessageBox;
-using TextBox = System.Windows.Controls.TextBox;
+
 
 
 namespace CSharpProject.Views
@@ -15,7 +9,6 @@ namespace CSharpProject.Views
     public partial class MainWindow : Window
     {
     
-
         
         public MainWindow()
         {
@@ -24,33 +17,7 @@ namespace CSharpProject.Views
 
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-        //    if (cbCategory.SelectedItem != null)
-        //    {
-        //        var minUrl = tbURL.Text;
-                
-        //        TextBox flowName = tbFlowName;
-        //        var name = tbFlowName.Text;
-        //        var category = cbCategory.SelectedItem.ToString();
-        //        var urlValidate = new UrlValidator();
-
-        //        if (urlValidate.Validate(minUrl) &&
-        //            urlValidate.EmptyTextBox(flowName) == true)
-        //        {
-        //            try
-        //            {
-        //                getFeeds.getRssItems(minUrl, name, category);
-        //                MessageBox.Show("Podcast tillagd!");
-        //            }
-        //            catch (Exception ez)
-        //            {
-        //                MessageBox.Show("Din url verkar inte gå att ladda. Felmeddelande:" + ez.Message);
-        //            }
-        //        }
-        //    }
-        //    else
-        //    {
-        //        MessageBox.Show("Du måste ange en kategori för att spara!");
-        //    }
+        
             new AddFeed().Show();
         }
 
@@ -59,13 +26,6 @@ namespace CSharpProject.Views
             new Window1().Show();
             
         }
-
-        //private void cbCategory_DropDownOpened_1(object sender, EventArgs e)
-        //{
-        //    cbCategory.Items.Clear();
-        //    ComboBox cbBoxBox = cbCategory;
-        //    fillCategories.GetCategory(cbBoxBox);
-        //}
 
         private void btnChangeFeed_Click(object sender, RoutedEventArgs e)
         {
